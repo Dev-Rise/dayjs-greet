03ee569aaec1dc29df71dbf6f027c59f06211d54
Marcin Rakowski <enamodeka@gmail.com>
2023-08-31 10:46:21 +0200

    chore: fix formatting

--- test/sl.test.js
+++ test/sl.test.js
@@ -6,20 +6,20 @@ const greetPlugin = require('../src')
 dayjs.extend(greetPlugin)
 
 it('Greet in SL', () => {
-    dayjs.locale('sl')
-    expect(dayjs('2022-07-07 04:00:00').greet()).toBe('Dobro jutro')
-    expect(dayjs('2022-07-07 09:59:59').greet()).toBe('Dobro jutro')
-    expect(dayjs('2022-07-07 09:59:59').greet(' custom suffix')).toBe(
-        'Dobro jutro custom suffix'
-    )
-    expect(dayjs('2022-07-07 10:00:00').greet()).toBe('Dober dan')
-    expect(dayjs('2022-07-07 19:59:59').greet()).toBe('Dober dan')
-    expect(dayjs('2022-07-07 19:59:59').greet(' custom suffix')).toBe(
-        'Dober dan custom suffix'
-    )
-    expect(dayjs('2022-07-07 20:00:00').greet()).toBe('Dober večer')
-    expect(dayjs('2022-07-07 03:59:59').greet()).toBe('Dober večer')
-    expect(dayjs('2022-07-07 03:59:59').greet(' custom suffix')).toBe(
-        'Dober večer custom suffix'
-    )
+  dayjs.locale('sl')
+  expect(dayjs('2022-07-07 04:00:00').greet()).toBe('Dobro jutro')
+  expect(dayjs('2022-07-07 09:59:59').greet()).toBe('Dobro jutro')
+  expect(dayjs('2022-07-07 09:59:59').greet(' custom suffix')).toBe(
+    'Dobro jutro custom suffix'
+  )
+  expect(dayjs('2022-07-07 10:00:00').greet()).toBe('Dober dan')
+  expect(dayjs('2022-07-07 19:59:59').greet()).toBe('Dober dan')
+  expect(dayjs('2022-07-07 19:59:59').greet(' custom suffix')).toBe(
+    'Dober dan custom suffix'
+  )
+  expect(dayjs('2022-07-07 20:00:00').greet()).toBe('Dober večer')
+  expect(dayjs('2022-07-07 03:59:59').greet()).toBe('Dober večer')
+  expect(dayjs('2022-07-07 03:59:59').greet(' custom suffix')).toBe(
+    'Dober večer custom suffix'
+  )
 })
