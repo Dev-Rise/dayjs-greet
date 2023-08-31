89482a8808275ba563f274cbe1c81f6bfa77dde7
Marcin Rakowski <enamodeka@gmail.com>
2023-08-31 12:43:20 +0200

    fix: fix sv-fi test

--- test/sv-fi.test.js
+++ test/sv-fi.test.js
@@ -7,14 +7,19 @@ dayjs.extend(greetPlugin)
 
 it('Greet in SV-FI', () => {
   dayjs.locale('sv-fi')
-  expect(dayjs('2022-07-07 04:00:00').greet()).toBe('Dobrý deň')
-  expect(dayjs('2022-07-07 17:59:59').greet()).toBe('Dobrý deň')
-  expect(dayjs('2022-07-07 17:59:59').greet(' custom suffix')).toBe(
-    'Dobrý deň custom suffix'
+  expect(dayjs('2022-07-07 04:00:00').greet()).toBe('God morgon')
+  expect(dayjs('2022-07-07 10:59:59').greet()).toBe('God morgon')
+  expect(dayjs('2022-07-07 10:59:59').greet(' custom suffix')).toBe(
+    'God morgon custom suffix'
   )
-  expect(dayjs('2022-07-07 18:00:00').greet()).toBe('Dobrý večer')
-  expect(dayjs('2022-07-07 03:59:59').greet()).toBe('Dobrý večer')
+  expect(dayjs('2022-07-07 11:00:00').greet()).toBe('Goddag')
+  expect(dayjs('2022-07-07 19:59:59').greet()).toBe('Goddag')
+  expect(dayjs('2022-07-07 19:59:59').greet(' custom suffix')).toBe(
+    'Goddag custom suffix'
+  )
+  expect(dayjs('2022-07-07 20:00:00').greet()).toBe('God kväll')
+  expect(dayjs('2022-07-07 03:59:59').greet()).toBe('God kväll')
   expect(dayjs('2022-07-07 03:59:59').greet(' custom suffix')).toBe(
-    'Dobrý večer custom suffix'
+    'God kväll custom suffix'
   )
 })
