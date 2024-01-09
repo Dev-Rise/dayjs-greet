a1f98c4d6abd0d89d44ca15237fbfc80000243c1
Marcin Rakowski <enamodeka@gmail.com>
2024-01-09 11:07:04 +0100

    fix: fix sk locale morning greeting

--- src/locale/sk.js
+++ src/locale/sk.js
@@ -1,7 +1,7 @@
 module.exports = (i) => {
   const hour = i.hour()
   if (hour >= 4 && hour < 12) {
-    return 'Dobro jutro%s' // (4:00 - 11:59)
+    return 'Dobré ráno%s' // (4:00 - 11:59)
   }
   if (hour >= 12 && hour < 18) {
     return 'Dober dan%s' // (12:00 - 17:59)
--- test/sk.test.js
+++ test/sk.test.js
@@ -7,10 +7,10 @@ dayjs.extend(greetPlugin)
 
 it('Greet in SK', () => {
   dayjs.locale('sk')
-  expect(dayjs('2022-07-07 04:00:00').greet()).toBe('Dobro jutro')
-  expect(dayjs('2022-07-07 11:59:59').greet()).toBe('Dobro jutro')
+  expect(dayjs('2022-07-07 04:00:00').greet()).toBe('Dobré ráno')
+  expect(dayjs('2022-07-07 11:59:59').greet()).toBe('Dobré ráno')
   expect(dayjs('2022-07-07 11:59:59').greet(' custom suffix')).toBe(
-    'Dobro jutro custom suffix'
+    'Dobré ráno custom suffix'
   )
   expect(dayjs('2022-07-07 12:00:00').greet()).toBe('Dober dan')
   expect(dayjs('2022-07-07 17:59:59').greet()).toBe('Dober dan')
