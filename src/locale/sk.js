2456f181b3d41880c55134d2ef2fe108ed2b5621
Marcin Rakowski <enamodeka@gmail.com>
2024-02-26 15:39:28 +0100

    fix: fix sk locale afternoon and evening greeting

--- src/locale/sk.js
+++ src/locale/sk.js
@@ -4,7 +4,7 @@ module.exports = (i) => {
     return 'Dobré ráno%s' // (4:00 - 11:59)
   }
   if (hour >= 12 && hour < 18) {
-    return 'Dober dan%s' // (12:00 - 17:59)
+    return 'Dobrý deň%s' // (12:00 - 17:59)
   }
-  return 'Dober večer%s'
+  return 'Dobrý večer%s'
 }
