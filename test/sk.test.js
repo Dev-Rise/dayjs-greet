73b7283835c53220610579784f0c62216dc0a131
Marcin Rakowski <enamodeka@gmail.com>
2024-02-26 15:39:28 +0100

    fix: fix sk locale afternoon and evening greeting

--- test/sk.test.js
+++ test/sk.test.js
@@ -12,14 +12,14 @@ it('Greet in SK', () => {
   expect(dayjs('2022-07-07 11:59:59').greet(' custom suffix')).toBe(
     'Dobré ráno custom suffix'
   )
-  expect(dayjs('2022-07-07 12:00:00').greet()).toBe('Dober dan')
-  expect(dayjs('2022-07-07 17:59:59').greet()).toBe('Dober dan')
+  expect(dayjs('2022-07-07 12:00:00').greet()).toBe('Dobrý deň')
+  expect(dayjs('2022-07-07 17:59:59').greet()).toBe('Dobrý deň')
   expect(dayjs('2022-07-07 17:59:59').greet(' custom suffix')).toBe(
-    'Dober dan custom suffix'
+    'Dobrý deň custom suffix'
   )
-  expect(dayjs('2022-07-07 18:00:00').greet()).toBe('Dober večer')
-  expect(dayjs('2022-07-07 03:59:59').greet()).toBe('Dober večer')
+  expect(dayjs('2022-07-07 18:00:00').greet()).toBe('Dobrý večer')
+  expect(dayjs('2022-07-07 03:59:59').greet()).toBe('Dobrý večer')
   expect(dayjs('2022-07-07 03:59:59').greet(' custom suffix')).toBe(
-    'Dober večer custom suffix'
+    'Dobrý večer custom suffix'
   )
 })
